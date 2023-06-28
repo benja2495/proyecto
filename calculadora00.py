a78c041b929096a46411376293940bb97dac5f3d
benja2495 <138017502+benja2495@users.noreply.github.com>
2023-06-28 09:42:08 -0400

    calculadora 1

---
+++ calculadora00.py
@@ -0,0 +1,23 @@
+
+
+# solicitar datos pro consola 
+
+numero1=float(input("introducir el primer numero:"))
+numero2=float(input("introducir el segundo numero:"))
+
+
+# imprimir operatoria 
+
+print("la suma es: ", numero1+numero2)
+print("la resta es: ", numero1-numero2)
+print("la division es: ", numero1/numero2)
+print("la multiplicacion es: ", numero1*snumero2)
+# este imput mantiene 
+input()
+
+
+
+
+
+if __name__ == '__main__':
+	main()
